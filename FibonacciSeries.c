2f6a53087c4896d992a84ed1571c869292a44112
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2020-10-07 14:54:35 +0530

    Create FibonacciSeries.c

---
+++ FibonacciSeries.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void main() {
+    
+    int number, f3;
+    printf("please enter the number of Fibonacci numbers: \n");
+    scanf("%d", &number);
+    
+    number = number - 2; // As we are printing first two fibonnaci; 
+    
+    int f1 =0;
+    int f2 = 1;
+    printf( "%d\n%d\n", f1, f2);
+    
+    while(number--) {
+        
+        f3 = f1 + f2;
+        printf( "%d \n", f3);
+        f1 = f2;
+        f2 = f3;
+
+    }
+
+}

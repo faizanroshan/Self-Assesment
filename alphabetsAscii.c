0cab706a35dbc553b46a00fc2da409e741cfd38e
Shaik Faizan Roshan Ali <67701441+faizanroshan@users.noreply.github.com>
2020-10-15 11:44:40 +0530

    Create alphabetsAscii.c

---
+++ alphabetsAscii.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    
+    // This prints upperCase alphabets using ASCII values
+    for ( int ascii = 64; ascii <= 90; ascii++ ) {
+        
+        printf("%c ", ascii);
+    }
+    
+    // This prints lowerCase alphabets using ASCII values
+    for ( int ascii = 97; ascii <= 122; ascii++ ) {
+        
+        printf("%c ", ascii);
+    }
+    
+    
+}
